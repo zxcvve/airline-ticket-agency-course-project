40b81d6efb702c6301b200ea8df7893d272c988c
Serafim Lyovin <serafim.lyowin@yandex.ru>
2023-11-27 16:09:04 +0300

    initial authjs setup

---
+++ auth.ts
@@ -0,0 +1,81 @@
+import NextAuth from 'next-auth';
+import PostgresAdapter from "@auth/pg-adapter"
+import {Pool} from 'pg';
+import {authConfig} from "@/auth.config";
+import bcrypt from 'bcrypt';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: Number(process.env.DB_PORT),
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
+
+
+export const {handlers, auth, signIn, signOut} = NextAuth({
+  ...authConfig, adapter: PostgresAdapter(pool), providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+            .object({ email: z.string().email(), password: z.string().min(6) })
+            .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const {email, password} = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
+});
+
+
+// async function getUser(email: string): Promise<User | undefined> {
+//   try {
+//     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+//     return user.rows[0];
+//   } catch (error) {
+//     console.error('Failed to fetch user:', error);
+//     throw new Error('Failed to fetch user.');
+//   }
+// }
+
+
+// export const {auth, signIn, signOut} = NextAuth({
+//   ...authConfig,
+//   providers: [
+//     Credentials({
+//       async authorize(credentials) {
+//         const parsedCredentials = z
+//           .object({email: z.string().email(), password: z.string().min(6)})
+//           .safeParse(credentials);
+//
+//         if (parsedCredentials.success) {
+//           const {email, password} = parsedCredentials.data;
+//           const user = await getUser(email);
+//           if (!user) return null;
+//           const passwordsMatch = await bcrypt.compare(password, user.password);
+//
+//           if (passwordsMatch) return user;
+//         }
+//
+//         console.log('Invalid credentials');
+//         return null;
+//       },
+//     }),
+//   ],
+// });
