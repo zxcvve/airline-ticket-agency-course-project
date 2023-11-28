4ce44dac3255c95cb32255b987e9ad3164b05a55
Serafim Lyovin <serafim.lyowin@yandex.ru>
2023-11-29 01:45:04 +0300

    it kinda works after moving register function to route file

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,29 +1,46 @@
-// import {pool} from "@/app/lib/actions";
 import bcrypt from "bcrypt";
-// import {sql} from "slonik";
-import {Pool} from 'pg';
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
 
 export const dynamic = 'force-dynamic';
+import {pool} from "@/app/lib/db";
+import {z} from "zod";
 
-export async function POST(request: Request) {
-  const {email, password} = await request.json();
-  const hashedPassword = await bcrypt.hash(password, 10);
+async function validateCredentials(email: string, password: string) {
+  const parsedCredentials = z
+    .object({email: z.string().email(), password: z.string().min(6)})
+    .safeParse({email, password});
+
+  if (!parsedCredentials.success) {
+    console.log("Invalid credentials", parsedCredentials.error);
+    return false;
+  }
+  return parsedCredentials.data;
+}
+
+export async function registerUser(email: string, password: string) {
+  const credentialsVaild = await validateCredentials(email, password);
+
+  if(!credentialsVaild) {
+    return false;
+  }
+
+  const hashedPassword = await bcrypt.hash(credentialsVaild.password, 10);
 
   try {
-    const query = "INSERT INTO users (email, password) values ($1, $2)"
-    const result = await pool.query(query,[email, hashedPassword])
+    const query: string = "INSERT INTO users (email, password) values ($1, $2)"
+    const result = await pool.query(query,[credentialsVaild.email, hashedPassword])
 
     return result.rowCount == 1;
   } catch (err) {
     console.error("Error registering user", err);
     return false;
+  }
+}
+export async function POST(request: Request) {
+  const {email, password} = await request.json();
+  const res = await registerUser(email, password);
+  return {
+    body: {
+      success: res,
+    },
   }
 }
