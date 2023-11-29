d5a98e4a54a0cdfd3a24c909da4b76e5c6d0bdcf
Serafim Lyovin <serafim.lyowin@yandex.ru>
2023-11-29 14:12:11 +0300

    imported functions finally work as expected

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,46 +1,12 @@
-import bcrypt from "bcrypt";
+import {NextResponse} from "next/server";
+import {registerUser} from "@/app/lib/actions";
 
 export const dynamic = 'force-dynamic';
-import {pool} from "@/app/lib/db";
-import {z} from "zod";
 
-async function validateCredentials(email: string, password: string) {
-  const parsedCredentials = z
-    .object({email: z.string().email(), password: z.string().min(6)})
-    .safeParse({email, password});
 
-  if (!parsedCredentials.success) {
-    console.log("Invalid credentials", parsedCredentials.error);
-    return false;
-  }
-  return parsedCredentials.data;
-}
-
-export async function registerUser(email: string, password: string) {
-  const credentialsVaild = await validateCredentials(email, password);
-
-  if(!credentialsVaild) {
-    return false;
-  }
-
-  const hashedPassword = await bcrypt.hash(credentialsVaild.password, 10);
-
-  try {
-    const query: string = "INSERT INTO users (email, password) values ($1, $2)"
-    const result = await pool.query(query,[credentialsVaild.email, hashedPassword])
-
-    return result.rowCount == 1;
-  } catch (err) {
-    console.error("Error registering user", err);
-    return false;
-  }
-}
 export async function POST(request: Request) {
   const {email, password} = await request.json();
   const res = await registerUser(email, password);
-  return {
-    body: {
-      success: res,
-    },
-  }
+
+  return NextResponse.json({res})
 }
