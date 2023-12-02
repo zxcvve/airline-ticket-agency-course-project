caff7750faa8fc72ae5a05eddfbeb1a86ffa8beb
Serafim Lyovin <serafim.lyowin@yandex.ru>
2023-12-02 22:08:34 +0300

    login form edits

--- app/components/login-form.tsx
+++ app/components/login-form.tsx
@@ -2,20 +2,30 @@
 
 import {Button} from '@nextui-org/button'
 import {Input} from "@nextui-org/input";
+import {useFormState} from 'react-dom';
+import {handleLogin} from "@/app/lib/handlers"
+
+
+
 
 export default function LoginForm() {
+  const [formState, formAction] = useFormState(handleLogin, undefined);
+
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-4xl font-bold">Вход</h1>
         <div className="flex flex-col justify-center items-center">
-          <form className="flex flex-col justify-center items-center">
-            <Input className="p-2" type="email" placeholder="Логин"/>
-            <Input className="p-2" type="password" placeholder="Пароль"/>
-            <Button className="">Войти</Button>
+          <form className="flex flex-col justify-center items-center" action={formAction}>
+            <Input className="p-2" type="email" id="email" name="email" placeholder="Логин"/>
+
+            <Input className="p-2" type="password" id="password" name="password" placeholder="Пароль"/>
+            <Button className="" type="submit">Войти</Button>
           </form>
         </div>
       </div>
     </div>
+
   );
 }
