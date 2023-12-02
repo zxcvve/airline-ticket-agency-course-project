28e7dd0b9a66ef1d3f8ebf6e9b6d8744c95f8e10
Serafim Lyovin <serafim.lyowin@yandex.ru>
2023-12-02 22:09:12 +0300

    init handlers

---
+++ app/lib/handlers.ts
@@ -0,0 +1,13 @@
+"use server"
+
+import {loginUser, registerUser} from "@/app/lib/actions";
+
+// server actions axample: https://github.com/vercel/next.js/blob/canary/examples/next-forms/app/actions.ts#L12
+// TODO: implement login and register handlers
+
+
+export async function handleLogin(prevState: any, formData: FormData) {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  console.log(email, password);
+}
