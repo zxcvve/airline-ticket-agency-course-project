73fc07a995d8225aef998f84098412e563444bc1
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-03 23:13:30 +0300

    added some admin pages

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,6 +1,22 @@
+import { Link } from "@nextui-org/link";
+
+
 export default function AdminPage() {
   return <>
-    <h1>Admin</h1>
-    <p>Admin page</p>
+    <Link href="admin/users">Пользователи</Link>
+    <br></br>
+    <Link href="admin/routes">Маршруты</Link>
+    <br></br>
+    <Link href="admin/flights">Полёты</Link>
+    <br></br>
+    <Link href="admin/airports">Аэропорты</Link>
+    <br></br>
+    <Link href="admin/airplanes">Самолёты</Link>
+    <br></br>
+    <Link href="admin/tickets">Билеты</Link>
+    <br></br>
+    <Link href="admin/priecs">Цены</Link>
+    <br></br>
+    <Link href="admin/seats">Места</Link>
   </>;
 }
