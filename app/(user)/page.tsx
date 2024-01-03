153179f8f984ace505b34108e40d31aa73ffcc06
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-03 23:14:57 +0300

    fetch flight list on page load, display flight list

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,12 +1,30 @@
 "use client";
+import { flight } from "../lib/definitions";
+import { useEffect, useState } from "react";
 import FlightSearch from "../components/flight-search-bar";
+import { getFlightList } from "../lib/actions";
 
 import { Button } from "@nextui-org/button";
+import FlightList from "../components/flight-list";
+
+
 
 export default function Home() {
+  const [flights, setFlights] = useState<flight[]>([]);
+
+  useEffect(() => {
+    async function getFlights() {
+      const flights = await getFlightList();
+      setFlights(flights);
+    }
+    getFlights();
+  }, []);
+
+
   return (
     <>
       <FlightSearch />
+      <FlightList flights={flights as flight[]} />
     </>
   );
 }
