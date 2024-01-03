68b539ac3674425e367632510e4bad297373fc9b
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-03 22:25:36 +0300

    added useRouter

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/system";
+import { useRouter } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
   return <NextUIProvider>{children}</NextUIProvider>;
 }
