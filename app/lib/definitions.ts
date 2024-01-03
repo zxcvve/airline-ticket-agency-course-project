9af5943967a87837ff6debab27370926966d00b9
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-03 23:13:47 +0300

    fixed interface name capitalization

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,6 +1,6 @@
 type Gender = "Male" | "Female" | "Other";
 
-export interface User {
+export interface user {
   id: number;
   last_name: string;
   first_name: string;
@@ -13,14 +13,14 @@ export interface User {
   password: string;
 }
 
-export interface Airplane {
+export interface airplane {
   id: number;
   model: string;
   reg_number: string;
   seats: number;
 }
 
-export interface Flight {
+export interface flight {
   id: number;
   flight_number: number;
   departure_time: Date;
@@ -29,14 +29,14 @@ export interface Flight {
   route: number;
 }
 
-export interface Route {
+export interface route {
   id: number;
   departure_airport: number;
   arrival_airport: number;
   flight_duration: string;
 }
 
-export interface TicketPrice {
+export interface ticketPrice {
   id: number;
   flight: number;
   price_starts: Date;
@@ -44,7 +44,7 @@ export interface TicketPrice {
   price: number;
 }
 
-export interface Ticket {
+export interface ticket {
   id: number;
   passanger: number;
   flight: number;
@@ -52,7 +52,7 @@ export interface Ticket {
   seat: string;
 }
 
-export interface Airport {
+export interface airport {
   id: number;
   title: string;
   city: string;
