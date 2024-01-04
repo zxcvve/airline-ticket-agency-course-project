818b9253557c482c25b10882b988492e410c609c
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-04 14:40:46 +0300

    removed flight search for now

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,30 +1,11 @@
-"use client";
-import { flight } from "../lib/definitions";
-import { useEffect, useState } from "react";
 import FlightSearch from "../components/flight-search-bar";
-import { getFlightList } from "../lib/actions";
-
-import { Button } from "@nextui-org/button";
 import FlightList from "../components/flight-list";
 
-
-
-export default function Home() {
-  const [flights, setFlights] = useState<flight[]>([]);
-
-  useEffect(() => {
-    async function getFlights() {
-      const flights = await getFlightList();
-      setFlights(flights);
-    }
-    getFlights();
-  }, []);
-
-
+export default async function Home() {
   return (
     <>
-      <FlightSearch />
-      <FlightList flights={flights as flight[]} />
+      {/* <FlightSearch /> */}
+      <FlightList />
     </>
   );
 }
