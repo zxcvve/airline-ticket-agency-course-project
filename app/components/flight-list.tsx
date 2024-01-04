ffede11348719a4863a0ec540e20655a158e0613
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-04 14:40:21 +0300

    Flight list is server side rendered now

--- app/components/flight-list.tsx
+++ app/components/flight-list.tsx
@@ -1,13 +1,17 @@
-import { flight } from "../lib/definitions";
-import FlightItem from './flight'
+"use server";
 
-export default function FlightList({ flights }: { flights: flight[] }) {
+import { getFlightList } from "../lib/actions";
+import { flight } from "../lib/definitions";
+import FlightItem from "./flight";
+
+export default async function FlightList() {
+  const flights = await getFlightList();
+
   return (
     <div>
       {flights.map((flight: flight) => {
-        return <FlightItem key={flight.id} {...flight} />
+        return <FlightItem key={flight.id} {...flight} />;
       })}
     </div>
-
-  )
+  );
 }
