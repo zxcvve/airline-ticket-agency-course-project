cbd33c2cfbd6b4bf062bcc596869f00cd5da4053
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-04 20:31:06 +0300

    added seat selection

--- app/components/new-order-form.tsx
+++ app/components/new-order-form.tsx
@@ -3,12 +3,34 @@
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import { Divider } from "@nextui-org/divider";
 import { Button } from "@nextui-org/button";
-import { FlightInfo } from "../lib/definitions";
+import { FlightInfo, Seat } from "../lib/definitions";
 import { Input } from "@nextui-org/input";
 import { redirectToLogin } from "@/app/(user)/lib/actions";
+import { Select, SelectItem } from "@nextui-org/react";
 
 import { SessionData, defaultSession } from "@/app/lib/session";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+import { getFreeSeats, newOrder } from "../lib/actions";
+
+function SeatSelect(props: any) {
+  const [seats, setSeats] = useState<Seat[]>([]);
+
+  useEffect(() => {
+    getFreeSeats(props.flight_id,50).then((seats) => {
+      setSeats(seats);
+    });
+  }, [props.flight_id]);
+
+  return (
+    <Select label="Место" isRequired items={seats}>
+      {(seat) => (
+        <SelectItem key={seat.seat_id} textValue={String(seat.seat_number)}>
+          {seat.seat_number}
+        </SelectItem>
+      )}
+    </Select>
+  );
+}
 
 export function NewOrderForm(flight: FlightInfo) {
   const [session, setSession] = useState<SessionData>(defaultSession);
@@ -39,15 +61,15 @@ export function NewOrderForm(flight: FlightInfo) {
 }
 
 function Form(props: any) {
-  const flight = props.flight;
-  const session = props.session;
-
-  const userId = session.userId;
+  const flight: FlightInfo = props.flight;
+  const session: SessionData = props.session;
 
   const flight_departure_date = flight.departure_time.toLocaleDateString();
   const flight_arrival_date = flight.arrival_time.toLocaleDateString();
   const flight_departure_time = flight.departure_time.toLocaleTimeString();
   const flight_arrival_time = flight.arrival_time.toLocaleTimeString();
+
+  const price_number: number = Number(flight.current_price.substring(1));
 
   return (
     <Card className="" isHoverable={true}>
@@ -65,8 +87,12 @@ function Form(props: any) {
         </p>
       </CardBody>
       <CardFooter className="justify-center">
-        <Input placeholder="ФИО" />
-        <Button onClick={() => console.log(session.userId)}>
+        <SeatSelect flight_id={flight.flight_id} />
+        <Button
+          onClick={() =>
+            newOrder(session.userId, flight.flight_id, price_number, 1)
+          }
+        >
           Купить {flight.current_price}
         </Button>
       </CardFooter>
