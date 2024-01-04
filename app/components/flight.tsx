2285e9658d3f99451f21ebebec4999f30430a3fc
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-04 19:40:51 +0300

    cleanup

--- app/components/flight.tsx
+++ app/components/flight.tsx
@@ -1,23 +1,36 @@
-import { flight } from "../lib/definitions";
-import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
+import { FlightInfo } from "../lib/definitions";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Button } from "@nextui-org/button";
+import { Divider } from "@nextui-org/divider";
+import { Link } from "@nextui-org/link";
 
+export default function FlightItem(flight: FlightInfo) {
+  const flight_departure_date = flight.departure_time.toLocaleDateString();
+  const flight_arrival_date = flight.arrival_time.toLocaleDateString();
 
-export default function FlightItem(flight: flight){
-
+  const flight_departure_time = flight.departure_time.toLocaleTimeString();
+  const flight_arrival_time = flight.arrival_time.toLocaleTimeString();
 
   return (
-    // <div>
-    //   <h3>{flight.id}</h3>
-    //   <p>{flight.departure_time.toString()}</p>
-    //   <p>{flight.arrival_time.toString()}</p>
-    // </div>
-    <Card>
-      <CardHeader>{flight.id}</CardHeader>
-      <CardBody>
-        <p>{flight.departure_time.toString()}</p>
-        <p>{flight.arrival_time.toString()}</p>
+    <Card className="" isHoverable={true}>
+      <CardBody className="flex flex-row ">
+        <p>{flight.airplane_model}</p>
+        <Divider orientation="vertical" className="px-4" />
+        <p>
+          {flight.departure_airport} <br />
+          {flight_departure_date} {flight_departure_time}
+        </p>
+        <Divider orientation="vertical" className="px-4" />
+        <p>
+          {flight.arrival_airport} <br />
+          {flight_arrival_date} {flight_arrival_time}
+        </p>
       </CardBody>
-      <CardFooter>Footer</CardFooter>
+      <CardFooter className="justify-center">
+        <Link href={`/new-order/${flight.flight_id}`}>
+          <Button>Купить {flight.current_price}</Button>
+        </Link>
+      </CardFooter>
     </Card>
-  )
+  );
 }
