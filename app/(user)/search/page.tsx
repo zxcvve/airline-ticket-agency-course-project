eb11f016250d2bc150dd0f21b6931ecaa15fa0b1
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-04 15:06:21 +0300

    initial search flight page

---
+++ app/(user)/search/page.tsx
@@ -0,0 +1,9 @@
+import FlightSearchBar from "../../components/flight-search-bar";
+
+export default function FlightSearch() {
+  return (
+    <div className="">
+      <FlightSearchBar />
+    </div>
+  )
+}
