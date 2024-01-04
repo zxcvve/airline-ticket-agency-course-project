da7670d8bfbf0a1b49d6da5f28ebef2ce080452e
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-04 19:38:57 +0300

    code cleanup

--- app/components/flight-list.tsx
+++ app/components/flight-list.tsx
@@ -1,16 +1,16 @@
 "use server";
 
-import { getFlightList } from "../lib/actions";
-import { flight } from "../lib/definitions";
+import { getFlightInfoList } from "../lib/actions";
+import { FlightInfo } from "../lib/definitions";
 import FlightItem from "./flight";
 
 export default async function FlightList() {
-  const flights = await getFlightList();
+  const flights = await getFlightInfoList();
 
   return (
     <div>
-      {flights.map((flight: flight) => {
-        return <FlightItem key={flight.id} {...flight} />;
+      {flights.map((flight: FlightInfo) => {
+        return <FlightItem key={flight.flight_id} {...flight} />;
       })}
     </div>
   );
