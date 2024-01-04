ee3fd5cd266a33e3e6ca8be67dee01125909aa30
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-04 14:57:08 +0300

    use root group specific navbar

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { Providers } from "../providers";
-import NavigationBar from "../components/navigation-bar";
+import NavigationBar from "./navigation-bar";
 
 const inter = Inter({ subsets: ["latin"] });
 
