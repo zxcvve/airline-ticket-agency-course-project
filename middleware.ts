fc89d58d6ee74d0a00d0864cef29b2469564e13e
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-08 22:49:06 +0300

    added comments

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,16 @@ import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { NextResponse, type NextRequest } from "next/server";
 import { SessionData, sessionOptions } from "./app/lib/session";
-import { redirect } from "next/dist/server/api-utils";
 
 export async function middleware(request: NextRequest) {
   const session = await getIronSession<SessionData>(cookies(), sessionOptions);
 
+  // редирект на страницу логина, если пользователь не авторизован и пытается зайти на страницу, требующую авторизации
   if (!session.isLoggedIn) {
     return NextResponse.redirect(new URL("/login", request.url), 302);
   }
 
+  // редирект на домашнюю страницу, если пользователь авторизован и пытается зайти на страницу админа
   if (
     request.nextUrl.pathname.startsWith("/admin") &&
     session.role !== "Admin"
@@ -20,5 +21,10 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/account/:path*", "/new-order/:path*", "/orders/:path*", "/admin/:path*"],
+  matcher: [
+    "/account/:path*",
+    "/new-order/:path*",
+    "/orders/:path*",
+    "/admin/:path*",
+  ],
 };
