b45dbc9ee50004634d545d84df3a1cd2782a47b4
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-09 17:13:36 +0300

    Added users table

--- app/(admin)/admin/users/page.tsx
+++ app/(admin)/admin/users/page.tsx
@@ -1,7 +1,48 @@
-export default function UserList() {
+import { getUsersList } from "@/app/lib/actions";
+import UsersTable from "./users-table";
+
+export default async function UserList() {
+  const users = await getUsersList();
+
+  const tableColumns = [
+    {
+      key: "id",
+      label: "ID",
+    },
+    {
+      key: "first_name",
+      label: "Имя",
+    },
+    {
+      key: "last_name",
+      label: "Фамилия",
+    },
+    {
+      key: "middle_name",
+      label: "Отчество",
+    },
+    {
+      key: "gender",
+      label: "Пол",
+    },
+    {
+      key: "phone_number",
+      label: "Телефон",
+    },
+    {
+      key: "email",
+      label: "Email",
+    },
+    {
+      key: "role",
+      label: "Роль",
+    }
+  ];
+
   return (
     <div>
       <h1>Список пользователей</h1>
+      <UsersTable users={users} columns={tableColumns} />
     </div>
   );
 }
---
+++ app/(admin)/admin/users/users-table.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { airport, userWithoutPassword } from "@/app/lib/definitions";
+import { getKeyValue } from "@nextui-org/react";
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+} from "@nextui-org/table";
+import Link from "next/link";
+
+interface processedUser extends userWithoutPassword {
+  gender: string;
+}
+
+export default function UsersTable(data: any) {
+  const columns: {
+    key: string;
+    label: string;
+  }[] = data.columns;
+
+  const rows: processedUser[] = data.users;
+
+  rows.map((row) => {
+    if (row.ismale) {
+      row.gender = "Мужской";
+    } else {
+      row.gender = "Женский";
+    }
+  });
+  
+  return (
+    <Table>
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => (
+              <TableCell>
+                <Link href={`users/${item.id}`}>
+                  {getKeyValue(item, columnKey)}
+                </Link>
+              </TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
