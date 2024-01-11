f7734a21df50e8cdc3ae7a784c62ad50ee5fcb36
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-11 23:09:11 +0300

    Update account edit page

---
+++ app/(user)/account/edit/edit-account-form.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import { updateUserInfo } from "@/app/lib/actions";
+import { UserWithoutPassword } from "@/app/lib/definitions";
+import {
+  Autocomplete,
+  AutocompleteItem,
+  Button,
+  Input,
+  Select,
+  SelectItem,
+} from "@nextui-org/react";
+import { useMemo, useState } from "react";
+
+function GenderAutocomplete({
+  genders,
+  fun,
+  label,
+  selectedKey,
+  name,
+}: {
+  genders: any[];
+  fun: any;
+  label: string;
+  selectedKey: number;
+  name: string;
+}) {
+  return (
+    <Autocomplete
+      label={label}
+      onSelectionChange={fun}
+      selectedKey={String(selectedKey)}
+      defaultItems={genders}
+    >
+      {(gender) => (
+        <AutocompleteItem key={gender.id}>{gender.name}</AutocompleteItem>
+      )}
+    </Autocomplete>
+  );
+}
+
+export default function EditUserInfoForm({
+  userData,
+}: {
+  userData: UserWithoutPassword;
+}) {
+  const genderList = [
+    { id: 0, name: "Женский" },
+    { id: 1, name: "Мужской" },
+  ];
+
+  const [firstName, setFirstName] = useState(userData.first_name);
+  const [secondName, setSecondName] = useState(userData.last_name);
+  const [middleName, setMiddleName] = useState(userData.middle_name);
+  const [phoneNumber, setPhone] = useState(userData.phone_number);
+  const [gender, setGender] = useState(userData.ismale ? 1 : 0);
+  const [formError, setFormError] = useState<string>("");
+
+  const [formState, setFormState] = useState<string>("");
+
+  async function handleFormSubmission(event: any) {
+    event.preventDefault();
+
+    setFormError("");
+    setFormState("");
+
+    if (phoneIsInvalid) {
+      setFormError("Неверный формат номера телефона");
+      return;
+    }
+
+    if (
+      firstName === "" ||
+      secondName === "" ||
+      middleName === "" ||
+      phoneNumber === ""
+    ) {
+      setFormError("Заполните все поля");
+      return;
+    }
+
+    const isMale = Number(gender) === 1 ? true : false;
+
+    const res = await updateUserInfo(
+      userData.id,
+      firstName,
+      secondName,
+      middleName,
+      phoneNumber,
+      isMale,
+    );
+
+    setFormState(res);
+  }
+
+  const phoneIsInvalid = useMemo(() => {
+    const regex = /^(\+7|8)(\d{10})$/;
+    return !regex.test(phoneNumber);
+  }, [phoneNumber]);
+
+  return (
+    <form className="flex flex-col gap-2" onSubmit={handleFormSubmission}>
+      <Input
+        type="text"
+        id="firstName"
+        name="firstName"
+        label="Имя"
+        value={firstName}
+        onValueChange={setFirstName}
+      />
+      <Input
+        type="text"
+        id="secondName"
+        name="secondName"
+        label="Фамилия"
+        value={secondName}
+        onValueChange={setSecondName}
+      />
+      <Input
+        type="text"
+        id="middleName"
+        name="middleName"
+        label="Отчество"
+        value={middleName}
+        onValueChange={setMiddleName}
+      />
+      <Input
+        type="text"
+        id="phoneNumber"
+        name="phoneNumber"
+        label="Номер телефона"
+        isInvalid={phoneIsInvalid}
+        value={phoneNumber}
+        onValueChange={setPhone}
+      />
+
+      <GenderAutocomplete
+        genders={genderList}
+        fun={setGender}
+        selectedKey={gender}
+        label="Пол"
+      />
+
+      <Button type="submit">Сохранить</Button>
+      {formState && (
+        <>
+          <p className="text-sm text-green-500">{formState}</p>
+        </>
+      )}
+      {formError && (
+        <>
+          <p className="text-sm text-red-500">{formError}</p>
+        </>
+      )}
+    </form>
+  );
+}
--- app/(user)/account/edit/page.tsx
+++ app/(user)/account/edit/page.tsx
@@ -1,151 +1,33 @@
-"use client";
+import { UserWithoutPassword } from "@/app/lib/definitions";
+import { cookies } from "next/headers";
+import EditUserInfoForm from "./edit-account-form";
 
-import { SessionData, defaultSession, sessionOptions } from "@/app/lib/session";
-import { Button } from "@nextui-org/button";
-import { Input } from "@nextui-org/input";
-import { Select, SelectItem } from "@nextui-org/select";
-import { useEffect, useState } from "react";
-import { getUserInfo, updateUserInfo } from "@/app/lib/actions";
-import { redirectToLogin } from "../../lib/actions";
-
-export default function EditUserInfo() {
-  const [session, setSession] = useState<SessionData>(defaultSession);
-  const [isLoading, setIsLoading] = useState(true);
-  const [userInfo, setUserInfo] = useState<any>([]);
-
-  useEffect(() => {
-    fetch("/api/auth/session")
-      .then((res) => res.json())
-      .then((session) => {
-        setSession(session);
-        getUserInfo(session.userId).then((userData) => {
-          setUserInfo(userData);
-          setIsLoading(false);
-        });
-      });
-  }, []);
-  if (isLoading) {
-    return <p>Загрузка...</p>;
+export default async function EditAccountPage(){
+  async function getUrl() {
+    const url =
+      process.env.NODE_ENV === "production"
+        ? process.env.URL
+        : "http://localhost:3000";
+    return url;
   }
+  const url = await getUrl();
 
-  if (session.isLoggedIn) {
-    const props = {
-      userInfo,
-      session,
-    };
-    return <Form {...props} />;
+  async function getCookie(name: string) {
+    return cookies().get(name)?.value ?? "";
   }
+  const cookie = await getCookie("air-app-session");
 
-  redirectToLogin();
-}
+  const userData: UserWithoutPassword = await fetch(`${url}/api/auth/me`, {
+    headers: {
+      Cookie: `air-app-session=${cookie};`,
+    },
+    next: {
+      tags: ["userData"],
+    },
+  }).then((res) => res.json());
 
-function Form(props: any) {
-  const [firstName, setFirstName] = useState<string>("");
-  const [secondName, setSecondName] = useState<string>("");
-  const [middleName, setMiddleName] = useState<string>("");
-  const [phone, setPhone] = useState<string>("");
-  const [gender, setGender] = useState<any>("");
+  return(
+    <EditUserInfoForm userData={userData}/>
+  )
 
-  const [result, setResult] = useState<any>("");
-
-  const genderList = [
-    { id: 0, name: "Женский" },
-    { id: 1, name: "Мужской" },
-  ];
-
-  useEffect(() => {
-    setFirstName(props.userInfo.first_name);
-    setSecondName(props.userInfo.last_name);
-    setMiddleName(props.userInfo.middle_name);
-    setPhone(props.userInfo.phone_number);
-    setGender(props.userInfo.ismale ? ["1"] : ["0"]);
-  }, []);
-
-  async function handleSubmit(event: any) {
-    event.preventDefault();
-    const firstName = event.target[0].value;
-    const secondName = event.target[1].value;
-    const middleName = event.target[2].value;
-    const phone = event.target[3].value;
-    const isMale = Number(event.target[4].value) ? true : false;
-
-
-    const session = await fetch("/api/auth/session").then((res) => res.json());
-    const res = await updateUserInfo(
-      session.userId,
-      firstName,
-      secondName,
-      middleName,
-      phone,
-      isMale,
-    );
-    setResult(res);
-  }
-
-  return (
-    <div className="">
-      <form onSubmit={handleSubmit}>
-        <Input
-          className="p-2"
-          type="text"
-          id="firstName"
-          name="firstName"
-          placeholder="Имя"
-          value={firstName}
-          onValueChange={setFirstName}
-        />
-        <Input
-          className="p-2"
-          type="text"
-          id="secondName"
-          name="secondName"
-          placeholder="Фамилия"
-          value={secondName}
-          onValueChange={setSecondName}
-        />
-        <Input
-          className="p-2"
-          type="text"
-          id="middleName"
-          name="middleName"
-          placeholder="Отчество"
-          value={middleName}
-          onValueChange={setMiddleName}
-        />
-        <Input
-          className="p-2"
-          type="tel"
-          id="phone"
-          name="phone"
-          placeholder="Номер телефона"
-          value={phone}
-          onValueChange={setPhone}
-        />
-        <Select
-          id="gender"
-          selectedKeys={gender}
-          onSelectionChange={setGender}
-          className="p-2"
-          label="Пол"
-        >
-          {genderList.map((gender) => (
-            <SelectItem key={gender.id}>{gender.name}</SelectItem>
-          ))}
-        </Select>
-
-        <div className="flex justify-center">
-          <Button className="w-fit flex justify-center" type="submit">
-            Сохранить
-          </Button>
-        </div>
-        <div className="justify-center flex p-4">
-          {result && (
-            <>
-              <p className="text-sm text-green-500">{result}</p>
-            </>
-          )}
-        </div>
-      </form>
-    </div>
-  );
 }
