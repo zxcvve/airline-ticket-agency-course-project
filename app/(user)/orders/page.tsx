7bdb605333cf0036f5ff08fe2effd7f5f09e95b5
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-11 21:15:48 +0300

    Add link to order page

--- app/(user)/orders/page.tsx
+++ app/(user)/orders/page.tsx
@@ -8,6 +8,7 @@ import { cookies } from "next/headers";
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import { Divider } from "@nextui-org/divider";
 import { redirect } from "next/navigation";
+import  Link  from "next/link";
 
 async function getSession() {
   const session = await getIronSession<SessionData>(cookies(), sessionOptions);
@@ -16,14 +17,6 @@ async function getSession() {
 }
 
 function Order(order: TicketInfo) {
-  // return (
-  //   <div>
-  //     <h1>{order.ticket_id}</h1>
-  //     <p>{order.departure_airport}</p>
-  //     <p>{order.arrival_airport}</p>
-  //   </div>
-  // );
-
   const flight_departure_date = new Date(
     order.departure_time,
   ).toLocaleDateString();
@@ -35,24 +28,26 @@ function Order(order: TicketInfo) {
   const flight_arrival_time = new Date(order.arrival_time).toLocaleTimeString();
 
   return (
-    <Card className="m-2" isHoverable={true}>
-      <CardBody className="flex flex-row ">
-        <p>
-          {order.ticket_id} <br />
-          {order.airplane_model}
-        </p>
-        <Divider orientation="vertical" className="px-4" />
-        <p>
-          {order.departure_airport} <br />
-          {flight_departure_date} {flight_departure_time}
-        </p>
-        <Divider orientation="vertical" className="px-4" />
-        <p>
-          {order.arrival_airport} <br />
-          {flight_arrival_date} {flight_arrival_time}
-        </p>
-      </CardBody>
-    </Card>
+    <Link href={`orders/${order.ticket_id}`}>
+      <Card className="m-2" isHoverable={true}>
+        <CardBody className="flex flex-row ">
+          <p>
+            {order.ticket_id} <br />
+            {order.airplane_model}
+          </p>
+          <Divider orientation="vertical" className="px-4" />
+          <p>
+            {order.departure_airport} <br />
+            {flight_departure_date} {flight_departure_time}
+          </p>
+          <Divider orientation="vertical" className="px-4" />
+          <p>
+            {order.arrival_airport} <br />
+            {flight_arrival_date} {flight_arrival_time}
+          </p>
+        </CardBody>
+      </Card>
+    </Link>
   );
 }
 
