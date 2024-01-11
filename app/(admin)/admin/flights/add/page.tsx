e8d0bc1926d5f2e39d54d11c4493c7555cea8972
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-11 19:08:05 +0300

    Change flight addition screen to display only enabled airplanes

--- app/(admin)/admin/flights/add/page.tsx
+++ app/(admin)/admin/flights/add/page.tsx
@@ -1,9 +1,9 @@
-import { getAirplanes, getAirports, getRoutes } from "@/app/lib/actions";
+import { getAirplanes, getAirports, getEnabledAirplanes, getRoutes } from "@/app/lib/actions";
 import AddFlightForm from "./add_flight_form";
 import { Airplane, Airport, RouteInfo } from "@/app/lib/definitions";
 
 export default async function AddFlightPage() {
-  const airplanes: Airplane[] = await getAirplanes();
+  const airplanes: Airplane[] = await getEnabledAirplanes();
   const routes: RouteInfo[] = await getRoutes();
   const data = {
     airplanes,
