0743647428bd36210f88ee38cb93cb4d0c7f7b6c
Serafim Lyovin <serafim.lyowin@yandex.ru>
2024-01-11 19:55:27 +0300

    Styling fix

--- app/components/flight-list.tsx
+++ app/components/flight-list.tsx
@@ -8,7 +8,7 @@ export default async function FlightList() {
   const flights = await getAvailableFlightsInfoList();
 
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       {flights.map((flight: FlightInfo) => {
         return <FlightItem key={flight.flight_id} {...flight} />;
       })}
